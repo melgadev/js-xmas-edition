c1b20fc37284b8e9ba4c8f8e2d2ca75874852772
melgadev <melgarejo.developer@gmail.com>
2023-10-19 13:08:45 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,11 +5,6 @@ const ciudad = $form.ciudad.value;
 const comportamiento = $form.comportamiento.value;
 const descripcionRegalo = $form['descripcion-regalo'].value;
 
-console.log(nombre);
-console.log(ciudad);
-console.log(comportamiento);
-console.log(descripcionRegalo);
-
 function validarNombre(nombre) {
     if (nombre.length === 0) {
         return 'Éste campo debe tener al menos 1 caracter';
